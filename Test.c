95ee18ee232b461061a7f0c9e991149170884291
hchouhan <hchouhan@visteon.com>
2021-11-25 18:06:42 +0530

    add mult func 2

--- Test.c
+++ Test.c
@@ -4,9 +4,12 @@
 int main()
 {
 int add=0;
+int mult=0;
 
 printf(" Creating first git file \n");
 add= add_numbers(2,3);
+
+mult=mult_numbers(3,4);
 printf ("\n ADDITION %d \n", add):
 printf(" push from local to remote \n");
 return 0;
---
+++ mult.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+
+int mult_numbers( int a , int b)
+{
+
+printf("Multiply a and b\n");
+return (a*b);
+}
