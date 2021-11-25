248a65462c9d08fcaec2c2dd5c0dc01fbd28401a
hchouhan <hchouhan@visteon.com>
2021-11-25 17:55:53 +0530

    add function

--- Test.c
+++ Test.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+int add=0;
 
 printf(" Creating first git file \n");
-
+add= add_numbers(2,3);
+printf ("\n ADDITION %d \n", add):
 printf(" push from local to remote \n");
 return 0;
 }
---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+
+int add_numbers( int a , int b)
+{
+
+printf("Adding a and b\n");
+return (a+b);
+}
